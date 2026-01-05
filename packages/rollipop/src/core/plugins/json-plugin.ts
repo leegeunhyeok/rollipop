819a39150ab7afcd847fd2a109e4fbc127d7fc3d
leegeunhyeok <dev.ghlee@gmail.com>
2026-01-06 02:56:03 +0900

    feat(json): cjs compatibility

--- packages/rollipop/src/core/plugins/json-plugin.ts
+++ packages/rollipop/src/core/plugins/json-plugin.ts
@@ -1,21 +1,26 @@
+import fs from 'node:fs';
+
 import { id, include } from '@rolldown/pluginutils';
 import type * as rolldown from 'rolldown';
-import { viteJsonPlugin } from 'rolldown/experimental';
 
 import { setFlag, TransformFlag } from './utils/transform-flags';
 
-function jsonPlugin(): rolldown.Plugin[] {
-  const jsonMarker: rolldown.Plugin = {
-    name: 'rollipop:json-marker',
-    transform: {
+function jsonPlugin(): rolldown.Plugin {
+  return {
+    name: 'rollipop:json',
+    load: {
       filter: [include(id(/\.json$/))],
       handler(id) {
-        return { meta: setFlag(this, id, TransformFlag.SKIP_ALL) };
+        const rawJson = fs.readFileSync(id, 'utf-8');
+
+        return {
+          code: `export = ${rawJson};`,
+          meta: setFlag(this, id, TransformFlag.SKIP_ALL),
+          moduleType: 'ts',
+        };
       },
     },
   };
-
-  return [jsonMarker, viteJsonPlugin({ namedExports: true })];
 }
 
 export { jsonPlugin as json };
