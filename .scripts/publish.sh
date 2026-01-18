8f7a392048424664a9f3141a92a2546cd55f133e
leegeunhyeok <dev.ghlee@gmail.com>
2026-01-18 15:58:52 +0900

    ci: add tag for publish

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+if [ -z "$1" ]; then
+  echo "No tag provided. using latest"
+  publish_tag="latest"
+else
+  publish_tag=$1
+fi
+
+echo "Publishing with tag: $publish_tag"
+
 # Publish @rollipop/*
-yarn workspaces foreach --all --no-private --include="@rollipop/*" exec yarn npm publish --provenance --access public --tolerate-republish
+yarn workspaces foreach --all --no-private --include="@rollipop/*" exec yarn npm publish --provenance --access public --tolerate-republish --tag $publish_tag
 
 # FIXME
 # When deploying with yarn workspaces foreach, an OIDC authentication error occurs only in the `rollipop` package.
@@ -11,7 +20,7 @@ yarn workspaces foreach --all --no-private --include="@rollipop/*" exec yarn npm
 version=$(cat packages/rollipop/package.json | jq -r '.version')
 if ! yarn npm info rollipop@$version --fields version --json | jq -r '.version' | grep -q $version; then
   yarn workspace rollipop pack --out package.tgz
-  npm publish packages/rollipop/package.tgz --tag latest --provenance
+  npm publish packages/rollipop/package.tgz --tag $publish_tag --provenance
 else
   echo "rollipop@$version is already published"
 fi
