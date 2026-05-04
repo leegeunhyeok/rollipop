b9357d66616eb63d45c24675c444ddb251276897
leegeunhyeok <dev.ghlee@gmail.com>
2026-05-05 04:36:13 +0900

    setup

--- examples/0.72/rollipop.config.ts
+++ examples/0.72/rollipop.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'rollipop';
 
-
-const noopHmrClient = `
-module.exports = { default: {
-  setup() {}, enable() {}, disable() {},
-  registerBundle() {}, log() {},
-} };
-`;
-
 export default defineConfig({
-  entry: 'index.js',
   runtimeTarget: 'hermes',
   devMode: {
-    hmr: { clientImplement: noopHmrClient },
+    hmr: false,
   },
 });
